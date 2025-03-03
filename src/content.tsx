7233abc39c5aef0e65e8b3c2a6f406ce0fc4cfe4
muhi111 <107345594+muhi111@users.noreply.github.com>
2025-03-03 09:00:37 +0900

    feat: add CI workflow and update project configuration
    - Added a CI workflow for code quality checks using Biome.
    - Updated package.json to include a new check script.
    - Added .vscode to .gitignore.
    - Cleaned up code formatting in various files.

--- src/content.tsx
+++ src/content.tsx
@@ -1,11 +1,11 @@
-import cssText from "data-text:~style.css"
-import type { PlasmoCSConfig } from "plasmo"
+import cssText from "data-text:~style.css";
+import type { PlasmoCSConfig } from "plasmo";
 
-import { CountButton } from "~features/count-button"
+import { CountButton } from "~features/count-button";
 
 export const config: PlasmoCSConfig = {
-  matches: ["<all_urls>"]
-}
+	matches: ["<all_urls>"],
+};
 
 /**
  * Generates a style element with adjusted CSS to work correctly within a Shadow DOM.
@@ -20,29 +20,29 @@ export const config: PlasmoCSConfig = {
  *    regardless of the host page's font size.
  */
 export const getStyle = (): HTMLStyleElement => {
-  const baseFontSize = 16
+	const baseFontSize = 16;
 
-  let updatedCssText = cssText.replaceAll(":root", ":host(plasmo-csui)")
-  const remRegex = /([\d.]+)rem/g
-  updatedCssText = updatedCssText.replace(remRegex, (match, remValue) => {
-    const pixelsValue = parseFloat(remValue) * baseFontSize
+	let updatedCssText = cssText.replaceAll(":root", ":host(plasmo-csui)");
+	const remRegex = /([\d.]+)rem/g;
+	updatedCssText = updatedCssText.replace(remRegex, (match, remValue) => {
+		const pixelsValue = Number.parseFloat(remValue) * baseFontSize;
 
-    return `${pixelsValue}px`
-  })
+		return `${pixelsValue}px`;
+	});
 
-  const styleElement = document.createElement("style")
+	const styleElement = document.createElement("style");
 
-  styleElement.textContent = updatedCssText
+	styleElement.textContent = updatedCssText;
 
-  return styleElement
-}
+	return styleElement;
+};
 
 const PlasmoOverlay = () => {
-  return (
-    <div className="plasmo-z-50 plasmo-flex plasmo-fixed plasmo-top-32 plasmo-right-8">
-      <CountButton />
-    </div>
-  )
-}
+	return (
+		<div className="plasmo-z-50 plasmo-flex plasmo-fixed plasmo-top-32 plasmo-right-8">
+			<CountButton />
+		</div>
+	);
+};
 
-export default PlasmoOverlay
+export default PlasmoOverlay;
